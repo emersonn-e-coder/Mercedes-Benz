cf6918aa69d203d725064762940f7e6f86ae31a7
Emerson-e-coder <63274859+Emerson-e-coder@users.noreply.github.com>
2020-04-16 16:33:09 -0300

    Mercedes

---
+++ script.js
@@ -0,0 +1,59 @@
+let theToggle = document.getElementById('toggle');
+
+// JS do menu
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	let newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	let newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
+/*==================================================================================================*/ 
+
+/*JS mudaCor*/
+function prata (){
+    document.getElementById("trocaimagem").src="imagens/mrced-prata.jpg";
+}
+
+function ouro (){
+    document.getElementById("trocaimagem").src="imagens/mercd-ouro.jpg";
+}
+function vermelho (){
+    document.getElementById("trocaimagem").src="imagens/mercd-verm.jpg";
+}
+function azul (){
+    document.getElementById("trocaimagem").src="imagens/mercedes-azul.jpg";
+}
+
+
